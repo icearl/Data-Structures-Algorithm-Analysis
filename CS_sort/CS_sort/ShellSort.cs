7cb3a7a4a83c6f63880644629664a6561e0c2eba
icearl <icearl@qq.com>
2017-07-30 17:53:59 +0800

    希尔排序

--- CS_sort/CS_sort/ShellSort.cs
+++ CS_sort/CS_sort/ShellSort.cs
@@ -13,18 +13,20 @@ namespace CS_sort {
             // gap为步长，每次减为原来的一半。
             for (gap = len / 2; gap > 0; gap /= 2) {
                 // 遍历 gap 个组，对每一组都执行直接插入排序
-                for (i = 0; i < gap; i++) {
+                for (i = 0; i <= gap - 1; i++) {
                     // 对每组进行插入排序
-                    for (j = i + gap; j < len; j += gap) {
+                    for (j = i + gap; j <= len - 1; j += gap) {
                         // 如果a[j] < a[j-gap]，则寻找a[j]位置，并将后面数据的位置都后移。
                         if (a[j] < a[j - gap]) {
-                            int tmp = a[j];
+                            int temp = a[j];
                             int k = j - gap;
-                            while (k >= 0 && a[k] > tmp) {
-                                a[k + gap] = a[k];
-                                k -= gap;
+                            while (k >= 0) {
+                                if(a[k] > temp) {
+                                    a[k + gap] = a[k];
+                                    k -= gap;
+                                }
                             }
-                            a[k + gap] = tmp;
+                            a[k + gap] = temp;
                         }
                     }
                 }
