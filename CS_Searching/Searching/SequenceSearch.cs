e4421bab2679a82b47e398ddaa806c84491d806b
icearl <icearl@qq.com>
2017-07-30 22:12:48 +0800

    线性查找

--- CS_Searching/Searching/SequenceSearch.cs
+++ CS_Searching/Searching/SequenceSearch.cs
@@ -3,13 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+//一种无序（不要求有序）查找算法
 namespace Searching {
-    class SequenceSearch {
+    class SequenceSearch { 
         public int Search(int[] array, int key) {
-            for (int index = 0; index < array.Length; index++) {
-                if (array[index] == key)
-                    return index;
+            for (int i = 0; i < array.Length; i++) {
+                if (array[i] == key)
+                    return i;
             }
             return -1;
         }
