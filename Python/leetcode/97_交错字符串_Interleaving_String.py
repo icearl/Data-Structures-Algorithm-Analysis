817c81ff96bc98fa67775481eaf7d8172439897c
icearl <icearl@qq.com>
2018-05-14 13:16:00 +0800

    update

--- Python/leetcode/97_交错字符串_Interleaving_String.py
+++ Python/leetcode/97_交错字符串_Interleaving_String.py
@@ -18,4 +18,34 @@ class Solution:
     @hint: you can use [[True] * m for i in range (n)] to allocate a n*m matrix.
     """
     def isInterleave(self, s1, s2, s3):
+        """
+        :type s1: str
+        :type s2: str
+        :type s3: str
+        :rtype: bool
+        """
+        if s1 is None or s2 is None or s3 is None:
+            return False
+        if len(s1) + len(s2) != len(s3):
+            return False
+        # i + 1 * j + 1 维的存储 T/F 的矩阵
+        interleave = [[False] * (len(s2) + 1) for i in range(len(s1) + 1)]
+        # 初始化左上角
+        interleave[0][0] = True
+        # 初始化左边和上边的边
+        for i in range(len(s1)):
+            interleave[i + 1][0] = s1[:i + 1] == s3[:i + 1]
+        for i in range(len(s2)):
+            interleave[0][i + 1] = s2[:i + 1] == s3[:i + 1]
+        # 状态转移方程
+        for i in range(len(s1)):
+            for j in range(len(s2)):
+                # 先初始化为不行
+                interleave[i + 1][j + 1] = False
+                # 如果用 i ，那就看上一个走的 j 是不是可以
+                if s1[i] == s3[i + j + 1]:
+                    interleave[i + 1][j + 1] = interleave[i][j + 1]
+                if s2[j] == s3[i + j + 1]:
+                    interleave[i + 1][j + 1] |= interleave[i + 1][j]
+        return interleave[len(s1)][len(s2)]
 
