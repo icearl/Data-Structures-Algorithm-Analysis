6368a3116ca30ffad11e62d796ff7acc25c7a2da
icearl <icearl@qq.com>
2018-08-09 22:18:19 +0800

    fix:som

--- Python/target_offer/_python_file/22_从上往下打印二叉树.py
+++ Python/target_offer/_python_file/22_从上往下打印二叉树.py
@@ -1,5 +1,5 @@
 '''
-书：23题
+书：32题
 从上往下打印出二叉树的每个节点，同层节点从左至右打印。(打印到一行就行)
 '''
 
@@ -14,14 +14,13 @@ class TreeNode:
         self.left = None
         self.right = None
 class Solution:
+    # 1. 不分行从上到下打印二叉树
     # 返回从上到下每个节点值列表，例：[1,2,3]
-    def PrintFromTopToBottom(self, root):
-        queue = []
+    def PrintFromTopToBottom1(self, root):
         if not root:
             return []
-
+        queue = [root]
         result = []
-        queue.append(root)
         while len(queue) > 0:
             currentRoot = queue.pop(0)
             result.append(currentRoot.val)
@@ -30,6 +29,50 @@ class Solution:
             if currentRoot.right is not None:
                 queue.append(currentRoot.right)
         return result
+
+    # 2. 分行，从上到下打印二叉树
+    def PrintFromTopToBottom2(self, root):
+        # write code here
+        if root is None:
+            return []
+        nodes = [root]
+        res = []
+        while nodes != []:
+            # 不要用 cur_val = next_nodes = [] ,这样会让 cur_val 引向 next_nodes
+            cur_val, next_nodes = [], []
+            for i in nodes:
+                cur_val.append(i.val)
+                if i.left is not None:
+                    next_nodes.append(i.left)
+                if i.right is not None:
+                    next_nodes.append(i.right)
+            res.append(cur_val)
+            nodes = next_nodes
+        return res
+
+    # 3. 分行，从上到下，之字型打印二叉树
+    def PrintFromTopToBottom3(self, root):
+        # write code here
+        if root is None:
+            return []
+        nodes = [root]
+        res = []
+        left_to_right = True
+        while nodes != []:
+            # 不要用 cur_val = next_nodes = [] ,这样会让 cur_val 引向 next_nodes
+            cur_val, next_nodes = [], []
+            for i in nodes:
+                cur_val.append(i.val)
+                if i.left is not None:
+                    next_nodes.append(i.left)
+                if i.right is not None:
+                    next_nodes.append(i.right)
+            if not left_to_right:
+                cur_val.reverse()
+            res.append(cur_val)
+            left_to_right = not left_to_right
+            nodes = next_nodes
+        return res
 
 pNode1 = TreeNode(8)
 pNode2 = TreeNode(6)
@@ -47,4 +90,4 @@ pNode3.left = pNode6
 pNode3.right = pNode7
 
 S = Solution()
-print(S.PrintFromTopToBottom(pNode1))
+print(S.PrintFromTopToBottom3(pNode1))
