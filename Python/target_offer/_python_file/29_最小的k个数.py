8ae9bcfa3a3bb722e68e0f51c367d56d2d1c9d30
icearl <icearl@qq.com>
2018-08-12 23:06:09 +0800

    fix:28

--- Python/target_offer/_python_file/29_最小的k个数.py
+++ Python/target_offer/_python_file/29_最小的k个数.py
@@ -1,7 +1,18 @@
-'''
+"""
 书:40
 输入n个整数，找出其中最小的K个数。例如输入4,5,1,6,2,7,3,8这8个数字，则最小的4个数字是1,2,3,4。
-'''
+"""
+
+# -*- coding:utf-8 -*-
+# O(n) 可以改变数组
+class Solution:
+    def GetLeastNumbers_Solution(self, tinput, k):
+        # write code here
+        length = len(tinput)
+        if k > length:
+            return []
+        tinput = sorted(tinput)
+        return tinput[:k]
 
 class Solution:
     # O(n)(why?)的算法, 只有当我们可以修改输入的数组时可用
