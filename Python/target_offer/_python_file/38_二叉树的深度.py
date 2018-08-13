b52250d2e5c01b061669d4b642526f8a8d5f7fef
icearl <icearl@qq.com>
2018-08-13 22:29:14 +0800

    fix:38

--- Python/target_offer/_python_file/38_二叉树的深度.py
+++ Python/target_offer/_python_file/38_二叉树的深度.py
@@ -1,4 +1,4 @@
-'''
+'''书：55
 输入一棵二叉树，求该树的深度。
 从根结点到叶结点依次经过的结点（含根、叶结点）形成树的一条路径，最长路径的长度为树的深度。
 '''
@@ -10,8 +10,10 @@
 class Solution:
     def TreeDepth(self, pRoot):
         # write code here
-        if pRoot == None:
+        if pRoot is None:
             return 0
-        nLeft = self.TreeDepth(pRoot.left)
-        nRight = self.TreeDepth(pRoot.right)
-        return max(nLeft+1,nRight+1)
+
+        depth_left = self.TreeDepth(pRoot.left)
+        depth_right = self.TreeDepth(pRoot.right)
+
+        return 1 + max(depth_left, depth_right)
