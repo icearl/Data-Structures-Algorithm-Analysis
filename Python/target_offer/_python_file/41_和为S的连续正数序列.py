47e9579dd17e71db61382c42dd35187e183b10c1
icearl <icearl@qq.com>
2018-08-13 23:49:38 +0800

    fix:41

--- Python/target_offer/_python_file/41_和为S的连续正数序列.py
+++ Python/target_offer/_python_file/41_和为S的连续正数序列.py
@@ -1,7 +1,12 @@
-'''
+'''书：57
 找出所有和为S的连续正数序列
 输出所有和为S的连续正数序列。序列内按照从小至大的顺序，序列间按照开始数字从小到大的顺序
 '''
+
+"""
+思路：
+维护两端两个数
+"""
 class Solution:
     def FindContinuousSequence(self, tsum):
         # write code here
@@ -11,9 +16,10 @@ class Solution:
         big = 2
         # 加1后二进制的形式右移一位，相当于除2之后向上取整，
         # 因为在python2中只有除法中包括浮点数的时候，结果才会有小数，因此此时不能用math.ceil()（向上取整函数）
-        middle = (tsum + 1) // 2
+        middle = int((tsum + 1) / 2)
         curSum = small + big
         output = []
+        # 因为大于 mid 的两个数的和就会比 tsum 大了
         while small < middle:
             if curSum == tsum:
                 output.append(range(small, big + 1))
