ddadd748e8e5b1cc194f1f3a56c44b3347192f15
icearl <icearl@qq.com>
2018-08-20 23:40:42 +0800

    fix:58

--- Python/target_offer/58_对称的二叉树.py
+++ Python/target_offer/58_对称的二叉树.py
@@ -13,6 +13,11 @@
 注意，如果一个二叉树同此二叉树的镜像是同样的，定义其为对称的。
 """
 
+"""
+递归完事了
+1、 基本情况
+2、 假设得到了子问题的解，然后合并
+"""
 
 # -*- coding:utf-8 -*-
 # class TreeNode:
@@ -22,6 +27,12 @@
 #         self.right = None
 class Solution:
     def isSymmetrical1(self, pRoot1, pRoot2):
+        """
+        子函数
+        :param pRoot1:
+        :param pRoot2:
+        :return:
+        """
         if pRoot1 is None and pRoot2 is None:
             return True
         if pRoot1 is None or pRoot2 is None:  # 两者都为真的情况在上一条过滤掉了
@@ -33,5 +44,10 @@ class Solution:
         # 左子树的左支与右子树的右支比较 左子树的右支和右子树的左支比较
 
     def isSymmetrical(self, pRoot):
+        """
+        主函数
+        :param pRoot:
+        :return:
+        """
         # write code here
         return self.isSymmetrical1(pRoot, pRoot)
