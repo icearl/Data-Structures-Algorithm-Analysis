1c99d2e1307c290a54af5849c5a7c16600448c70
icearl <icearl@qq.com>
2018-08-20 21:03:53 +0800

    fix:18

--- Python/target_offer/56_删除链表中重复的结点.py
+++ Python/target_offer/56_删除链表中重复的结点.py
@@ -7,7 +7,7 @@
 @Software: PyCharm
 """
 
-"""
+"""书：18-2
 题：
 在一个排序的链表中，存在重复的结点，请删除该链表中重复的结点，重复的结点不保留，返回链表头指针。 
 例如，链表1->2->3->3->4->4->5 处理后为 1->2->5
@@ -37,7 +37,7 @@ class Solution:
         return pHead
 
 
-# 方法二：循环解法
+# 方法二：循环解法(用这个）
 # -*- coding:utf-8 -*-
 # class ListNode:
 #     def __init__(self, x):
@@ -60,3 +60,4 @@ class Solution:
             else:
                 p = p.next
         return nil.next
+
