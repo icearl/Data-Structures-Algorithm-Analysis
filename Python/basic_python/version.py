5c8b6ce23438f0b702b3bdff584291078d175177
icearl <icearl@qq.com>
2019-02-21 16:33:45 +0800

    fix:version;

---
+++ Python/basic_python/version.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+@Time    : 2019/2/21 4:03 PM
+@Author  : libing
+@Email   : icearl@qq.com
+@File    : version.py
+@Software: PyCharm
+"""
+
+"""
+python 的 2 和 3 版本不同
+不久 2 也不会再维护了，所以选 3 做新的项目
+但是有的项目要兼容 2，所以需要共存。
+下面是共存的方式：
+
+1. 两个都安装
+2. 配置环境变量，在环境变量路径里改变需要的名字
+    比如要用 python 调用 python3，那就复制一个 python3 改为 python
+3. 可以用 python / python2 / python3 进入交互模式检查是哪个版本的 python
+    可以用 which python 来检查 python 在什么路径 
+"""
+
